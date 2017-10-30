e8c37bceb5b902878fbe54f92232a4dccea651b6
James Wright <james@jamesswright.co.uk>
2017-10-30 18:16:13 +0000

    Configure snake casing JSON serialization

--- Startup.cs
+++ Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
 
 namespace GitHubAutoresponder {
     public class Startup {
@@ -19,7 +21,8 @@ namespace GitHubAutoresponder {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
-            services.AddMvc();
+            services.AddMvc()
+                .AddJsonOptions(ConfigureJsonSerialiser);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +32,14 @@ namespace GitHubAutoresponder {
             }
 
             app.UseMvc();
+        }
+
+        private void ConfigureJsonSerialiser(MvcJsonOptions options) {
+            DefaultContractResolver resolver = new DefaultContractResolver {
+                NamingStrategy = new SnakeCaseNamingStrategy(),
+            };
+
+            options.SerializerSettings.ContractResolver = resolver;
         }
     }
 }
