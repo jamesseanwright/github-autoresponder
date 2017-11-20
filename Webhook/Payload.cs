154038c974ca48fed3c26f7468512ccd5041b544
James Wright <james@jamesswright.co.uk>
2017-11-20 18:21:06 +0000

    TODO

--- Webhook/Payload.cs
+++ Webhook/Payload.cs
@@ -26,7 +26,7 @@ namespace GitHubAutoresponder.Webhook {
                     return PullRequest;
                 }
 
-                throw new CommentableException();
+                throw new CommentableException(); // TODO: model validation
             }
         }
     }
