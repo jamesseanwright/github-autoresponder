7624200b2e458bd84d3d32e7235ef5a2793e3164
James Wright <james@jamesswright.co.uk>
2017-11-23 18:49:58 +0000

    WIP WebhookControllerTests update

--- Webhook/Tests/WebhookControllerTests.cs
+++ Webhook/Tests/WebhookControllerTests.cs
@@ -4,28 +4,36 @@ using Xunit;
 using Moq;
 using GitHubAutoresponder.Responder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Internal;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using System.Net;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using GitHubAutoresponder.Shared;
+using System.IO;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace GitHubAutoresponder.Webhook.Tests {
-    public class WebhookControllerTests : IDisposable {
+    public class WebhookControllerTests {
         private Mock<IGitHubResponder> gitHubResponder;
         private Mock<IModelStateConverter> modelStateConverter;
         private Mock<IJsonSerialiser> jsonSerialiser;
         private Mock<IRequestValidator> requestValidator;
+        private Mock<IObjectModelValidator> objectValidator;
         private Mock<IEnvironment> environment;
         private WebhookController webhookController;
+        private Payload payload;
 
         public WebhookControllerTests() {
             this.gitHubResponder = new Mock<IGitHubResponder>();
             this.modelStateConverter = new Mock<IModelStateConverter>();
             this.jsonSerialiser = new Mock<IJsonSerialiser>();
             this.requestValidator = new Mock<IRequestValidator>();
+            this.objectValidator = new Mock<IObjectModelValidator>();
             this.environment = new Mock<IEnvironment>();
 
             this.webhookController = new WebhookController(
@@ -35,19 +43,50 @@ namespace GitHubAutoresponder.Webhook.Tests {
                 this.requestValidator.Object,
                 this.environment.Object
             );
-        }
 
-        public void Dispose() {
-            this.gitHubResponder = null;
+            this.webhookController.ControllerContext = new ControllerContext(
+                new ActionContext(
+                    new DefaultHttpContext(),
+                    new RouteData(),
+                    new ControllerActionDescriptor()
+                )
+            );
+
+            this.webhookController.ObjectValidator = this.objectValidator.Object;
         }
 
         [Fact]
         public async Task ItShouldForwardThePayload() {
+            string gitHubSignature = "signature";
+            string secret = "secret";
+            string body = "body";
             Payload payload = new Payload();
 
+            this.webhookController.Request.Headers.Add("X-Hub-Signature", gitHubSignature);
+            this.webhookController.Request.Body = new MemoryStream(Encoding.ASCII.GetBytes(body));
+
+            this.requestValidator
+                .Setup(r => r.IsValidRequest(gitHubSignature, secret, body))
+                .Returns(true);
+
+            this.objectValidator.Setup(o => o.Validate(
+                It.IsAny<ActionContext>(),
+                It.IsAny<ValidationStateDictionary>(),
+                It.IsAny<string>(),
+                It.IsAny<object>()
+            ));
+
+            this.jsonSerialiser
+                .Setup(g => g.Deserialise<Payload>(body))
+                .Returns(payload);
+
             this.gitHubResponder
-                .Setup(g => g.RespondAsync(It.IsAny<Payload>()))
+                .Setup(g => g.RespondAsync(payload))
                 .ReturnsAsync(true);
+
+            this.environment
+                .SetupGet(g => g.Secret)
+                .Returns(secret);
 
             ContentResult result = await this.webhookController.PostAsync();
 
@@ -56,33 +95,33 @@ namespace GitHubAutoresponder.Webhook.Tests {
             this.gitHubResponder.Verify(g => g.RespondAsync(payload), Times.Once());
         }
 
-        [Fact]
-        public async Task ItShouldRespondWithBadRequestWhenPayloadIsInvalid() {
-            Payload payload = new Payload();
+        // [Fact]
+        // public async Task ItShouldRespondWithBadRequestWhenPayloadIsInvalid() {
+        //     Payload payload = new Payload();
 
-            this.modelStateConverter
-                .Setup(m => m.AsString(this.webhookController.ModelState))
-                .Returns("Model validation errors");
+        //     this.modelStateConverter
+        //         .Setup(m => m.AsString(this.webhookController.ModelState))
+        //         .Returns("Model validation errors");
 
-            this.webhookController.ModelState.AddModelError("key", "Some model error");
+        //     this.webhookController.ModelState.AddModelError("key", "Some model error");
 
-            ContentResult result = await this.webhookController.PostAsync();
+        //     ContentResult result = await this.webhookController.PostAsync();
 
-            Assert.StrictEqual<int?>((int) HttpStatusCode.BadRequest, result.StatusCode);
-            Assert.Equal("Model validation errors", result.Content);
-        }
+        //     Assert.StrictEqual<int?>((int) HttpStatusCode.BadRequest, result.StatusCode);
+        //     Assert.Equal("Model validation errors", result.Content);
+        // }
 
-        [Fact]
-        public async Task ItShouldRespondWithBadGatewayWhenUpstreamReturnsError() {
-            Payload payload = new Payload();
+        // [Fact]
+        // public async Task ItShouldRespondWithBadGatewayWhenUpstreamReturnsError() {
+        //     Payload payload = new Payload();
 
-            this.gitHubResponder
-                .Setup(g => g.RespondAsync(It.IsAny<Payload>()))
-                .ReturnsAsync(false);
+        //     this.gitHubResponder
+        //         .Setup(g => g.RespondAsync(It.IsAny<Payload>()))
+        //         .ReturnsAsync(false);
 
-            ContentResult result = await this.webhookController.PostAsync();
+        //     ContentResult result = await this.webhookController.PostAsync();
 
-            Assert.StrictEqual<int?>((int) HttpStatusCode.BadGateway, result.StatusCode);
-        }
+        //     Assert.StrictEqual<int?>((int) HttpStatusCode.BadGateway, result.StatusCode);
+        // }
     }
 }
